260bd15e3cb794b01af1f22cc52a2fa4906dba24
Chaituvarma-109 <chaitanyavarma.u@gmail.com>
2024-10-29 10:07:55 +0530

    created inbox app and completed project.

--- a_core/settings.py
+++ a_core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django_htmx",
     'a_posts',
     'a_users',
+    'a_inbox',
 ]
 
 MIDDLEWARE = [
@@ -149,5 +150,5 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 
-ACCOUNT_USERNAME_BLACKLIST = [ 'admin', 'accounts', 'profile', 'category', 'post' ]
+ACCOUNT_USERNAME_BLACKLIST = [ 'admin', 'accounts', 'profile', 'category', 'post', 'inbox' ]
 ACCOUNT_SIGNUP_REDIRECT_URL = '/profile/onboarding/'
